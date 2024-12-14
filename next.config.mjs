4d603ad2556e2a78ebfff4922f5ee6a7162996ab
Castle9707 <paomincc25@gmail.com>
2024-12-14 16:35:08 +0800

    圖檔音檔設定檔

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  head: {
+    link: [
+      {
+        rel: "stylesheet",
+        href: "https://cdn.jsdelivr.net/npm/remixicon/fonts/remixicon.css",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
