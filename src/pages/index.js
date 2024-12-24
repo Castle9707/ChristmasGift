8adb5ca17a632f924342a400b3bd8546a243b48b
Castle9707 <paomincc25@gmail.com>
2024-12-24 22:47:08 +0800

    要一個名字

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,12 @@
 import Room from "@/components/room"
+import Head from "next/head"
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>嗨，請進｜城</title>
+      </Head>
       <Room />
     </>
   )
